e80ba8ed8aba0527aad795b0b512ab62c0c78cb2
dulabdul <dul.abdoel21@gmail.com>
2023-01-08 18:31:46 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import LandingPages from './pages/LandingPages';
 
-function App() {
+import React, { useEffect } from 'react';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route
+          exatch
+          path='/'
+          element={<LandingPages />}
+        />
+      </Routes>
+    </>
   );
 }
-
-export default App;
---
+++ src/parts/Header.jsx
@@ -0,0 +1,69 @@
+import { Navbar } from 'flowbite-react';
+import React from 'react';
+import logo from '../assets/images/design/logo.svg';
+import Button from '../components/Button';
+
+export default function Header() {
+  return (
+    <header className='w-full z-50 absolute'>
+      <Navbar
+        fluid={true}
+        rounded={true}
+        className='mx-auto container'>
+        <Navbar.Brand
+          href='/'
+          className=''>
+          <img
+            src={logo}
+            className='mr-3 h-12'
+            alt='Flowbite Logo'
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className='px-5 py-1'>
+          <li>
+            <Button
+              type='link'
+              href=''
+              className='text-primary text-base font-normal hover:text-secondary transition-all active'>
+              Why choose us
+            </Button>
+          </li>
+          <li>
+            <Button
+              type='link'
+              href=''
+              className='text-primary text-base font-normal hover:text-secondary transition-all'>
+              How it works
+            </Button>
+          </li>
+          <li>
+            <Button
+              type='link'
+              href=''
+              className='text-primary text-base font-normal hover:text-secondary transition-all'>
+              How to rent
+            </Button>
+          </li>
+          <li>
+            <Button
+              type='link'
+              href=''
+              className='text-primary text-base font-normal hover:text-secondary transition-all'>
+              Catch our deals
+            </Button>
+          </li>
+          <div className='border-r border-[#BEBEBE] '></div>
+          <li>
+            <Button
+              type='link'
+              href=''
+              className='text-primary text-base font-normal hover:text-secondary transition-all'>
+              Login
+            </Button>
+          </li>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  );
+}
